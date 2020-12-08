5078cf0098cf74321726723dcf11bb97503346d9
Yurii Savchuk <geo.savchuk@gmail.com>
2020-12-08 23:44:57 +0200

    Added event card and nav bar components

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <h1>Event Listing</h1>
-
-   <router-link :to="{ name: 'event-show', params: { id: '1' } }">Show Event #1</router-link>
+    <EventCard />
   </div>
 </template>
 
 <script>
-export default {
+import EventCard from '@/components/EventCard.vue'
 
+export default {
+  components: {
+    EventCard
+  }
 }
 </script>
